d93c486f151320173b2436be2e65ac49f018e35f
Lucas Araujo <lucasgdantas21@gmail.com>
2018-08-28 11:28:20 +0200

    Dimension

--- Second code.R
+++ Second code.R
@@ -1,4 +1,10 @@
 library(readr)
+setwd("D:/Lucas/UBIQUM/28-08")
+library(readr)
 LucasAndres <- read_delim("LucasAndres.csv", 
-                            +                           ";", escape_double = FALSE, trim_ws = TRUE)
-LucasAndres$X1 <- NULL
+                          ";", escape_double = FALSE, trim_ws = TRUE)
+View(LucasAndres)
+LucasAndres$Dimension <- LucasAndres$Depth*LucasAndres$Width*LucasAndres$Heigth
+LucasAndres$Depth <- NULL
+LucasAndres$Width <- NULL
+LucasAndres$Heigth <- NULL
